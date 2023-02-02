169018db803413a93ed45ef0e587cb5445029250
yosef hershkovitz <4195228@gmail.com>
2023-02-02 16:32:29 +0200

    add print_handling

--- DB.h
+++ DB.h
@@ -18,6 +18,7 @@ typedef struct
     int dept;
     Date date;
 } Costumer;
+typedef void (*PRINT_HANDLING)(int, char *);
 
 void create_list(FILE *file);
 char *compare_str(char *str, char *oper, unsigned int index, char *error_msg);
